c6e473b79aca4769fb1346e9439ef1606a09aec0
TRANTUANVU <vutran165@gmail.com>
2018-12-11 18:01:42 +0700

    1-11122018

--- webapp/src/main/java/com/ecommercial/webapp/controllers/HomeController.java
+++ webapp/src/main/java/com/ecommercial/webapp/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping("/login")
+    @RequestMapping("/")
     public String home(){
         return "index";
     }
